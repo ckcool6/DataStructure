9d02f1a38be2af8a060bc3becf829ce86cb430cb
rzdtjb <rzdtjb@gmail.com>
2023-04-22 17:08:29 +0800

    Set集合

--- src/GenericTest2.java
+++ src/GenericTest2.java
@@ -1,3 +1,21 @@
-public class GenericTest2 {
+import java.util.ArrayList;
 
+public class GenericTest2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+
+        printList(list1);
+        printList(list2);
+    }
+
+    private static void printList(ArrayList<?> list) {
+
+    }
+    private static void method1(ArrayList<? extends Number> list) {
+
+    }
+    private static void method2(ArrayList<? super Number> list) {
+
+    }
 }
